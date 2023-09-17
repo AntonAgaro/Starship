79b9695284a0e90385b8ebfcb69b45e599e7ea1c
user <burdenkof@yandex.ru>
2023-09-18 00:53:10 +0400

    forum && errors

--- packages/client/src/Routes/Router.tsx
+++ packages/client/src/Routes/Router.tsx
@@ -6,6 +6,10 @@ import LeaderBoard from '../Pages/LeaderBoard/leaderBoard'
 import SignIn from '../Pages/SignIn/signIn'
 import SignUp from '../Pages/SignUp/signUp'
 import Profile from '../Pages/Profile/profile'
+import CreateTopic from '../Pages/Forum/CreateTopic/createTopic'
+import Topic from '../Pages/Forum/Topic/topic'
+import Forum from '../Pages/Forum/forum'
+import Error from '../Pages/Error/error'
 
 export enum RouteUrls {
   landing = '/',
@@ -33,6 +37,13 @@ const Router = () => (
       <Route path={RouteUrls.signIn} element={<SignIn />} />
       <Route path={RouteUrls.signUp} element={<SignUp />} />
       <Route path={RouteUrls.profile} element={<Profile />} />
+
+      <Route path={RouteUrls.forum} element={<Forum />} />
+      <Route path={RouteUrls.createTopic} element={<CreateTopic />} />
+      <Route path={RouteUrls.topic} element={<Topic />} />
+
+      <Route path={RouteUrls.error404} element={<Error code={404} />} />
+      <Route path={RouteUrls.error500} element={<Error code={500} />} />
     </Route>
   </Routes>
 )
