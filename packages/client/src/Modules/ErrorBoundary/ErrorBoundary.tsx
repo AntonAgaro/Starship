0e65af279fcb9721246e71d05640ff1728382b62
Dmitry Karpekin <dkarpekin@twelvedevs.com>
2023-09-19 20:02:41 +0300

    added ErrorBoundary component

---
+++ packages/client/src/Modules/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,42 @@
+import { Alert, Space } from 'antd'
+import React, { useState, useEffect, ReactNode } from 'react'
+import './ErrorBoundary.less'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+}
+
+const ErrorBoundary = ({ children }: ErrorBoundaryProps) => {
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    const handleError = (error: ErrorEvent) => {
+      setError(error.error)
+    }
+
+    setError(null)
+
+    window.addEventListener('error', handleError)
+
+    return () => {
+      window.removeEventListener('error', handleError)
+    }
+  }, [])
+
+  if (error) {
+    return (
+      <Space direction="vertical" className="error-container">
+        <Alert
+          message={error?.message}
+          description="This is an error message"
+          type="error"
+          showIcon
+        />
+      </Space>
+    )
+  }
+
+  return <>{children}</>
+}
+
+export default ErrorBoundary
