6f85ef3d100993d019ae12525a738099ef92d2b5
“killank” <killankgg@gmail.com>
2023-09-20 15:52:21 +0300

    fix jest tests

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -7,7 +7,7 @@ export default {
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: {
     '\\.(css|less)$': '<rootDir>/mocks/cssStub.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/mocks/cssStub.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/mocks/cssStub.js',
   },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
