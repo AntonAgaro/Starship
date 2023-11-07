ca6142c07ce70e943a6892b6b40d4f80e76da505
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2023-11-08 01:26:47 +0500

    add forum types

---
+++ packages/client/src/Redux/forum/types.ts
@@ -0,0 +1,24 @@
+export type TCommentInfo = {
+  id: number
+  text: string
+  createdDateTime: string
+  author: TAuthorInfo
+  auhor_id: number
+}
+
+export type TAuthorInfo = {
+  first_name: string
+  second_name: string
+  display_name: string
+  login: string
+  id: number
+  avatar: string
+}
+
+export type TTopicInfo = {
+  id: number
+  title: string
+  created_date_time: string
+  last_comment_date_time: string | null
+  author: TAuthorInfo
+}
